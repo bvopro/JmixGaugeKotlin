005f48aee0665913bfd909d487adb167abbce4f4
Benoît VALLETTE d'OSIA <b.vallettedosia@altereo.fr>
2023-01-25 11:27:10 +0100

    initial

---
+++ src/main/kotlin/fr/altereo/jmixgaugekotlin/screen/user/UserBrowse.kt
@@ -0,0 +1,11 @@
+package fr.altereo.jmixgaugekotlin.screen.user
+
+import fr.altereo.jmixgaugekotlin.entity.User
+import io.jmix.ui.navigation.Route
+import io.jmix.ui.screen.*
+
+@UiController("User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+open class UserBrowse : StandardLookup<User>()
---
+++ src/test/kotlin/fr/altereo/jmixgaugekotlin/JmixGaugeKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.altereo.jmixgaugekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class JmixGaugeKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+
+    }
+}
